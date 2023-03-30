ea36dd9604d1da110e77b4d7300ec72c8da84039
Vivan Kumar <vivangkumar@gmail.com>
2023-03-30 17:35:57 +0100

    fix: make bucket concurrency safe
    - don't rely on the rate limiter to take care of safe access

--- internal/bucket/bucket.go
+++ internal/bucket/bucket.go
@@ -1,9 +1,14 @@
 package bucket
 
+import (
+	"sync"
+)
+
 // Bucket represents a token bucket.
 //
-// It is not safe for concurrent use.
+// It is safe for concurrent use.
 type Bucket struct {
+	m sync.Mutex
 	// size is the max tokens the bucket can hold.
 	size uint64
 
@@ -16,6 +21,7 @@ type Bucket struct {
 // size number of max tokens.
 func New(size uint64) *Bucket {
 	return &Bucket{
+		m:         sync.Mutex{},
 		size:      size,
 		available: size,
 	}
@@ -34,6 +40,9 @@ func (b *Bucket) Take() bool {
 // If n tokens are not available, no tokens are removed
 // from the bucket.
 func (b *Bucket) TakeN(n uint64) bool {
+	b.m.Lock()
+	defer b.m.Unlock()
+
 	if b.available >= n {
 		b.available -= n
 		return true
@@ -47,6 +56,9 @@ func (b *Bucket) TakeN(n uint64) bool {
 // If the quantity to refill exceeds the size of the bucket,
 // the bucket is refilled upto its size.
 func (b *Bucket) Refill(n uint64) {
+	b.m.Lock()
+	defer b.m.Unlock()
+
 	t := b.available + n
 	if t > b.size {
 		t = b.size
@@ -56,10 +68,16 @@ func (b *Bucket) Refill(n uint64) {
 
 // Available returns the tokens currently available in the bucket.
 func (b *Bucket) Available() uint64 {
+	b.m.Lock()
+	defer b.m.Unlock()
+
 	return b.available
 }
 
 // Size returns the max tokens a bucket can have.
 func (b *Bucket) Size() uint64 {
+	b.m.Lock()
+	defer b.m.Unlock()
+
 	return b.size
 }
